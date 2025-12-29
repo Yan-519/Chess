7e0bfeecd9892ee00033934c876ee79c9a230f1a
Yan <rozenberg.yan@gmail.com>
2025-12-29 17:24:50 +0200

    Move the Prev_move_memo class into Draw_data

--- Chess/Chess_classes.cs
+++ Chess/Chess_classes.cs
@@ -1,48 +1,48 @@
 ﻿namespace Chess;
 
-public class Prev_move_memo
-{
-    private const int SIZE = 12;
-    private Move[] moves = new Move[SIZE];
-
-    public Prev_move_memo()
-    {
-        for (int i = 0; i < SIZE; i++)
-            moves[i] = new();
-    }
-
-    private Prev_move_memo(Move[] moves)
-    {
-        for (int i = 0; i < SIZE; i++)
-            this.moves[i] = moves[i].copy();
-    }
-
-    public void Push(Move move)
-    {
-        for (int i = SIZE - 1; i > 0; i--)
-            moves[i] = moves[i - 1];
-
-        moves[0] = move.copy();
-    }
-
-    public bool is_repeated()
-    {
-        for (int i = 0; i < SIZE - 2; i++)
-            if (moves[i].from != moves[i + 2].to || moves[i + 2].is_None())
-                return false;
-        return true;
-    }
-
-    public Prev_move_memo Push_get(Move move)
-    {
-        Prev_move_memo prev = new(moves);
-        prev.Push(move);
-        return prev;
-    }
-}
-
 public class Draw_data
 {
+    internal class Prev_move_memo
+    {
+        private const int SIZE = 12;
+        private Move[] moves = new Move[SIZE];
+
+        public Prev_move_memo()
+        {
+            for (int i = 0; i < SIZE; i++)
+                moves[i] = new();
+        }
+
+        private Prev_move_memo(Move[] moves)
+        {
+            for (int i = 0; i < SIZE; i++)
+                this.moves[i] = moves[i].copy();
+        }
+
+        public void Push(Move move)
+        {
+            for (int i = SIZE - 1; i > 0; i--)
+                moves[i] = moves[i - 1];
+
+            moves[0] = move.copy();
+        }
+
+        public bool is_repeated()
+        {
+            for (int i = 0; i < SIZE - 2; i++)
+                if (moves[i].from != moves[i + 2].to || moves[i + 2].is_None())
+                    return false;
+            return true;
+        }
+
+        public Prev_move_memo Push_get(Move move)
+        {
+            Prev_move_memo prev = new(moves);
+            prev.Push(move);
+            return prev;
+        }
+    }
+
     private const int HALF_MOES_FOR_DRAW = 100;
 
     private Prev_move_memo prev_moves = new();
